1e14c910fb87cf44b10e2ea11a81d43fc1c3d708
Jonatan Moritz <jonatan.moritz@gmail.com>
2014-05-06 13:13:50 +0200

    salt

--- foodbook/app/Config/core.php
+++ foodbook/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', '15ef0a7ea9e3f49644402fe99fd7e43d6760a47c');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '8923562398562895687238095072580200');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
