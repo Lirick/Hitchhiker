4602ec34547fc83a32b0266a25adb8f1c922de7b
Lirick <martin.hastad@gmail.com>
2014-05-09 16:51:45 +0200

    Changed so that telefon number is a varchar rather than int

--- Tables/users.sql
+++ Tables/users.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `phone` int(28) NOT NULL,
+  `phone` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
