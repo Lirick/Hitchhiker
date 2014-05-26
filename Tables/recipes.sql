99b42be29a246fa2adf359f11dd6b59a09905567
Lirick <martin.hastad@gmail.com>
2014-05-26 19:24:24 +0200

    Forgot sql file

--- Tables/recipes.sql
+++ Tables/recipes.sql
@@ -1,3 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.6deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 26, 2014 at 05:20 PM
+-- Server version: 5.5.37-0ubuntu0.13.10.1
+-- PHP Version: 5.5.3-1ubuntu2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `foodbook`
+--
+
+-- --------------------------------------------------------
+
 --
 -- Table structure for table `recipes`
 --
@@ -6,10 +30,13 @@ CREATE TABLE IF NOT EXISTS `recipes` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `author` int(11) NOT NULL,
   `name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `ingreds` text COLLATE utf8_unicode_ci NOT NULL,
   `text` text COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
+  `date` date NOT NULL,
   `tags` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `sumratings` int(11) NOT NULL DEFAULT '0',
+  `nrratings` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=0 ;
 
 
